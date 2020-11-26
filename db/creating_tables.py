21aac2252b76b8249df6736b90578e98a5d546d4
PokoRoko <peuc386@gmail.com>
2020-11-27 00:15:37 +0300

    Added classes for db:orders

--- db/creating_tables.py
+++ db/creating_tables.py
@@ -1,12 +1,13 @@
-from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy import Column, Integer, String, Text, DateTime, Boolean, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
-
-engine = create_engine('sqlite:///tea.db', echo=False)
+# Определяем тип и название БД (echo параметр логирования)
+engine = create_engine('sqlite:///tea.db', echo=True)
+# Определяем базовый класс
 Base = declarative_base()
 
 
-# Создаем и определяем поля таблицы
+# Создаем и определяем поля для таблицы Пользователи
 class User(Base):
     """
     Класс для создания таблицы Продуктовой линейки с помощью SQlalchemy
@@ -24,16 +25,17 @@ class User(Base):
     login = Column(String)
     password = Column(String)
 
-    def __init__(self, telegram_id, name, login, password):
+    def __init__(self, telegram_id, name, login, password,):
         self.telegram_id = telegram_id
         self.name = name
         self.login = login
         self.password = password
 
     def __repr__(self):
-        return "<User('%s','%s', '%s', '%s')>" % (self.telegram_id, self.name, self.login, self.password)
+        return "<User('%s','%s', '%s', '%s')>" % (self.telegram_id, self.name, self.login, self.password,)
 
 
+# Создаем и определяем поля для таблицы Продукты
 class Product(Base):
     """
     Класс для создания таблицы Продуктовой линейки с помощью SQlalchemy
@@ -50,6 +52,7 @@ class Product(Base):
     description - Описани
     link_shipper - Ссылка на товар, на странице поставщика
     link_photo  - Ссылка на фото товара
+    photo - Фото
     """
 
     __tablename__ = 'products'
@@ -58,16 +61,17 @@ class Product(Base):
     category = Column(String)
     name = Column(String)
     shipper = Column(String)
-    price_shipper = Column(String)
-    price = Column(String)
+    price_shipper = Column(Integer)
+    price = Column(Integer)
     unit = Column(String)
     min_buy = Column(Integer)
-    description = Column(String)
+    description = Column(Text)
     link_shipper = Column(String)
     link_photo = Column(String)
+    photo = Column(Boolean)
 
     def __init__(self, article, category, name, shipper, price_shipper, price, unit,
-                 min_buy, description, link_shipper, link_photo):
+                 min_buy, description, link_shipper, link_photo, photo,):
         self.article = article
         self.category = category
         self.name = name
@@ -79,14 +83,58 @@ class Product(Base):
         self.description = description
         self.link_shipper = link_shipper
         self.link_photo = link_photo
+        self.photo = photo
 
     def __repr__(self):
         return (
             self.id, self.article, self.category, self.name, self.shipper,
             self.price_shipper, self.price, self.unit, self.min_buy,
-            self.description, self.link_shipper, self.link_photo,
+            self.description, self.link_shipper, self.link_photo, self.photo,
             )
 
+
+# Создаем и определяем поля для таблицы Заказы
+class Order(Base):
+    """
+    Класс для создания таблицы Заказов с помощью SQlalchemy
+    Описание:
+    id - Уникальный id пользователя
+    telegram_id - id из Телеграма если есть
+    name - Имя пользователя
+    login - Логин для входа
+    password  - Пароль для входа
+    """
+    __tablename__ = 'orders'
+    id = Column(Integer, primary_key=True)
+    id_user = Column(Integer)
+    date_create = Column(DateTime)
+    status = Column(String)
+    list_products = Column(String)
+    price_shipper = Column(Integer)
+    price = Column(Integer)
+    address_delivery = Column(Text)
+    type_delivery = Column(String)
+    date_delivery = Column(DateTime)
+
+    def __init__(self, id_user, date_create, status, list_products, price_shipper,
+                 price, address_delivery, type_delivery, date_delivery,
+                 ):
+
+        self.id_user = id_user
+        self.date_create = date_create
+        self.status = status
+        self.list_products = list_products
+        self.price_shipper = price_shipper
+        self.price = price
+        self.address_delivery = address_delivery
+        self.type_delivery = type_delivery
+        self.date_delivery = date_delivery
+
+    def __repr__(self):
+        return (
+            self.id_user, self.date_create, self.status, self.list_products, self.price_shipper,
+            self.price, self.address_delivery, self.type_delivery, self.date_delivery,
+        )
 
 Base.metadata.create_all(engine)  # Создает таблицы если их нет
 
